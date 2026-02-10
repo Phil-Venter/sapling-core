65d620f13817e9f6adaa863a2962c6e719382600
Philip <mrphilventer@gmail.com>
2026-02-10 16:31:37 +0200

    missed a spot

--- src/Template.php
+++ src/Template.php
@@ -11,6 +11,23 @@ final class Template
         self::$map[$key] = $value;
     }
 
+    public static function escape(mixed $value): string
+    {
+        $value = $value instanceof \Closure ? $value() : $value;
+
+        $value = match (true) {
+            is_null($value) => "",
+            is_float($value), is_int($value) => (string) $value,
+            is_string($value) => $value,
+            $value instanceof \BackedEnum => (string) $value->value,
+            $value instanceof \UnitEnum => $value->name,
+            $value instanceof \Stringable => (string) $value,
+            default => throw new \InvalidArgumentException("Invalid value type"),
+        };
+
+        return htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
+    }
+
     public static function render(string $template, iterable|object $vars = []): string
     {
         $map = self::$map;
@@ -18,16 +35,12 @@ final class Template
         foreach (self::flatten($vars) as $key => $value) {
             $map["unsafe:$key"] = $value;
             $map["url:$key"] = rawurlencode($value);
-            $map[$key] = e($value);
+            $map[$key] = self::escape($value);
         }
 
-        $map["dump"] = sprintf("<pre>%s</pre>", e(print_r($map, true)));
+        $map["dump"] = sprintf("<pre>%s</pre>", self::escape(print_r($map, true)));
 
-        return preg_replace_callback(
-            "/{{\s*([A-Za-z0-9_.:-]+)\s*}}/",
-            fn($match) => $map[$match[1]] ?? "<!-- {$match[0]} -->",
-            $template
-        );
+        return preg_replace_callback("/{{\s*([A-Za-z0-9_.:-]+)\s*}}/", fn($match) => $map[$match[1]] ?? "<!-- {$match[0]} -->", $template);
     }
 
     private static function flatten(iterable|object $data, string $prefix = ""): array
